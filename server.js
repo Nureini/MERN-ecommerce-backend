89b60e45d037bfb99340e2708377ec4bc132f154
Nureini <nureinimoobe@gmail.com>
2023-03-25 12:56:29 +0000

    updated server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const corsOptions = require("./config/corsOptions.js");
 
 const app = express();
 app.use(cookieParser());
+app.use(express.json());
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors(corsOptions));
